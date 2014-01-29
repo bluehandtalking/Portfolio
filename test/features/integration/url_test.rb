8d151ccf0cfcd8b09903d0888526ede237ba43b2
Jerrold Thompson <jet@whidbey.com>
2014-01-29 15:27:45 -0800

    Removed hyphen in front of ruby in .travis.yml

--- test/features/integration/url_test.rb
+++ test/features/integration/url_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-feature "When visiting my new cool site, check to be sure we are on the right page"do
+feature "When visiting my new cool site, check to be sure we are on the right page" do
   scenario "User choses to navigate to your cool site" do
     visit ( "http://www.ironhorserails.com/" )
     page.must_have_content("Being Here")
