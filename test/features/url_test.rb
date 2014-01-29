7eafdb04944a8c32cdab7fac54f7f2dc0a65e17c
Jerrold Thompson <jet@whidbey.com>
2014-01-28 19:33:00 -0800

    cleanup after returning to sqlite3

---
+++ test/features/url_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+feature  "As a parent, I want to see the good work my kid does so I can show it off to my friend" do
+  describe "When I go to the root url, my browser will navigate to the root URI" do
+    # uri = URI.parse(current_url) 
+    # current_url.should == root_url
+    # "#{uri.path}?#{uri.query}".should == people_path(:search => 'name') 
+  end
+end
