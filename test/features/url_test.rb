8f706997f2905d255e73d39ea184e9658e5af454
Jerrold Thompson <jet@whidbey.com>
2014-01-29 06:38:43 -0800

    Revert to previous

--- test/features/url_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-feature  "As a parent, I want to see the good work my kid does so I can show it off to my friend" do
-  describe "When I go to the root url, my browser will navigate to the root URI" do
-    # "#{uri.path}?#{uri.query}".should == people_path(:search => 'name') 
-  end
-end
