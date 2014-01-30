1583129d2ca4089cee918ed509816a770b9c8541
Jerrold Thompson <jet@whidbey.com>
2014-01-29 21:05:49 -0800

    Put / in for visiting cool site test

--- test/features/users/check_for_zurb_test.rb
+++ test/features/users/check_for_zurb_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 feature "CheckForZurb" do
   scenario "When visiting the page Zurb Foundation will be installed" do
     visit root_path
-    page.source.must_include("foundation_and_overrides.css") 
+    page.source.must_include("foundation") 
     page.wont_have_content "Goobye All!"
   end
 end
--- test/features/users/visiting_site_test.rb
+++ test/features/users/visiting_site_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
-feature "When visiting my new cool site, check to be sure we are on the right page" do
+feature "Visiting my new cool site, check to be sure we are on the right page" do
   scenario "User choses to navigate to your cool site" do
-    visit ( "http://www.ironhorserails.com/" )
+    visit  root_path 
     page.must_have_content("Being Here")
   end
 end
