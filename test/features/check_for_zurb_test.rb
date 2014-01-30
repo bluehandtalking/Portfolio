2999707be08744ec55415a47cde6db370f7aa9c9
Jerrold Thompson <jet@whidbey.com>
2014-01-29 17:12:52 -0800

    Changed to root_path for Zurb test

--- test/features/check_for_zurb_test.rb
+++ test/features/check_for_zurb_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 feature "CheckForZurb" do
   scenario "When visiting the page Zurb Foundation will be installed" do
-    visit ("http://www.ironhorserails.com")
+    visit root_path
     page.source.must_include("foundation_and_overrides.css") 
     page.wont_have_content "Goobye All!"
   end
