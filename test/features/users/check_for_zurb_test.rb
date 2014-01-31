c72c84c12c7fb78b66021cb26738ae56a7f477ae
Jerrold Thompson <jet@whidbey.com>
2014-01-30 19:05:02 -0800

    More cleanup

--- test/features/users/check_for_zurb_test.rb
+++
@@ -1,9 +0,0 @@
-require "test_helper"
-
-feature "CheckForZurb" do
-  scenario "When visiting the page Zurb Foundation will be installed" do
-    visit root_path
-    page.source.must_include("foundation") 
-    page.wont_have_content "Goobye All!"
-  end
-end
