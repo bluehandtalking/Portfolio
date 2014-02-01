1d0d3ebbbf4913af540bfa110f4b191bd729bcf7
Jerrold Thompson <jet@whidbey.com>
2014-02-01 11:51:40 -0800

    Check for user sign out test fixed

--- test/features/auth/sign_out_test.rb
+++ test/features/auth/sign_out_test.rb
@@ -8,10 +8,9 @@ feature "Allow a user who is signed in to be able to sign out" do
     visit "/users/sign_in"
     fill_in 'user_email', with: "chillwill@example.com" 
     fill_in 'Password', with: "secretpwd49"
-
-    # click_button "Sign in"
     page.find("[type='submit']").click 
-    # page.must_have_content( "Signed in successfully" )
-    # click_link "Sign out"
+    page.must_have_content( "Signed in successfully" )
+    click_link "Sign out"
+    page.must_have_content("Signed out successfully")
   end
 end
