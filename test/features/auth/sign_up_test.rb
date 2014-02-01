4c424b78f8ce3a5cde9f31898c2fc8db4627a507
Jerrold Thompson <jet@whidbey.com>
2014-02-01 12:44:10 -0800

    cleanup

--- test/features/auth/sign_up_test.rb
+++ test/features/auth/sign_up_test.rb
@@ -8,15 +8,11 @@ feature "A visitor comes to the site and signs up for a new account so I can per
     click_link "Sign up"
     # When I register with valid info
     fill_in 'Email', with: 'john@lighthouse.com'
-    # fill_in 'Password', with: users(:chill).encrypted_password  
     fill_in 'Password', with: 'password' 
-
     fill_in 'Password confirmation', with: 'password'
-    # click_button "Sign up"
     page.find("[type='submit']").click 
     # Then I should be signed up
     page.must_have_content( 'Welcome! You have signed up successfully' )
-   # page.should_have_selector ".notice", text: "Signed in successfully" 
     page.wont_have_content( 'You can not sign in as Zorro' )
   end
 end
