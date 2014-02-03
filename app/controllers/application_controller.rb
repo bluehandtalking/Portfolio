7bd7e68afe86f3594037d1b7b0721481cf76f280
Jerrold Thompson <jet@whidbey.com>
2014-02-02 18:06:08 -0800

    Pundit stable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include Pundit 
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized 
+
+  def user_not_authorized
+    flash[:error] = "You are not authorized to perform this action."
+    redirect_to request.headers["Referer"] || root_path
+  end
+
 end
---
+++ db/migrate/20140202211749_add_column_to_post.rb
@@ -0,0 +1,5 @@
+class AddColumnToPost < ActiveRecord::Migration
+  def change
+    add_column :posts, :author_id, :integer
+  end
+end
