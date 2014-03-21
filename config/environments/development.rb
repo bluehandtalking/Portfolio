af4df5dba2b49ec36b13a6db648265e18a233039
Jerrold Thompson <jet@whidbey.com>
2014-03-21 10:11:17 -0700

    Twitter auth working in dev after following modifications: 	modified:   app/models/user.rb 	modified:   config/application.yml 	modified:   config/environments/development.rb 	modified:   config/initializers/devise.rb 	modified:   config/initializers/secret_token.rb 	modified:   config/routes.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,4 +25,6 @@ Portfolio::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  # Devise mailer for dev environment
+  config.action_mailer.default_url_options = { host: 'localhost:3000' } 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Portfolio::Application.routes.draw do
 
   resources :welcomes
 
-  devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks"}
+  devise_for :users, path_names: {sign_in: 'login', sign_out: 'logout'},  controllers: {omniauth_callbacks: "omniauth_callbacks"}
   resources :posts do
     resources :comments
   end
