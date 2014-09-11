a84baed62d45c11b831e262e1684fd350682d63f
Jerrold Thompson <jet@whidbey.com>
2014-09-11 11:23:07 -0700

    Add published to projects table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140911180231) do
+ActiveRecord::Schema.define(version: 20140911182118) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20140911180231) do
     t.string   "description"
     t.string   "title"
     t.integer  "author_id"
+    t.boolean  "published"
   end
 
   create_table "users", force: true do |t|
