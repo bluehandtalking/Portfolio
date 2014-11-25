d1a2e8de0a7bd7908649cb53def84967d51a7098
Jerrold Thompson <jet@whidbey.com>
2014-11-25 11:30:18 -0800

    setting up post controller/model for nested attributes

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ActiveRecord::Base
   
   belongs_to :author, class_name: 'user' 
   has_many :comments, as: :commentable
+  accepts_nested_attributes_for :comments
 
   def publish!
     self.published = true
